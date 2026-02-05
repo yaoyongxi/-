a8ece8d3d2e98bfa876845a6ebd8a2724938178f
yaoyongxi <486337849@qq.com>
2026-02-05 22:20:13 +0800

    黑马面面1.1

---
+++ src/main/java/com/itheima/service/store/impl/CompanyServiceImpl.java
@@ -0,0 +1,181 @@
+package com.itheima.service.store.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.itheima.dao.store.CompanyDao;
+import com.itheima.domain.store.Company;
+import com.itheima.factory.MapperFactory;
+import com.itheima.service.store.CompanyService;
+import com.itheima.utils.TransactionUtil;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CompanyServiceImpl implements CompanyService {
+    @Override
+    public void save(Company company) {
+        SqlSession sqlSession = null;
+        try {
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取Dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession, CompanyDao.class);
+            //id使用UUID的生成策略来获取
+            String id = UUID.randomUUID().toString();
+            company.setId(id);
+            //3.调用Dao层操作
+            companyDao.save(company);
+            //4.提交事务
+            TransactionUtil.commit(sqlSession);
+
+        }catch (Exception e){
+            //回滚事务
+            TransactionUtil.rollback(sqlSession);
+            //将错误返回最上面
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try {
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                //打印异常
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void delete(Company company) {
+        SqlSession sqlSession = null;
+        try {
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取Dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession, CompanyDao.class);
+            //3.调用Dao层操作
+            companyDao.delete(company);
+            //4.提交事务
+            TransactionUtil.commit(sqlSession);
+
+        }catch (Exception e){
+            //回滚事务
+            TransactionUtil.rollback(sqlSession);
+            //将错误返回最上面
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try {
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                //打印异常
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void update(Company company) {
+        SqlSession sqlSession = null;
+        try {
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取Dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession, CompanyDao.class);
+            //3.调用Dao层操作
+            companyDao.update(company);
+            //4.提交事务
+            TransactionUtil.commit(sqlSession);
+
+        }catch (Exception e){
+            //回滚事务
+            TransactionUtil.rollback(sqlSession);
+            //将错误返回最上面
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try {
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                //打印异常
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public Company findById(String id) {
+        SqlSession sqlSession = null;
+        try {
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取Dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession, CompanyDao.class);
+            //3.调用Dao层操作
+            return companyDao.findById(id);
+        }catch (Exception e){
+            //将错误返回最上面
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try {
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                //打印异常
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public List<Company> findAll() {
+        SqlSession sqlSession = null;
+        try {
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取Dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession, CompanyDao.class);
+            //3.调用Dao层操作
+            return companyDao.findAll();
+        }catch (Exception e){
+            //将错误返回最上面
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try {
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                //打印异常
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public PageInfo findAll(int page, int size) {
+        SqlSession sqlSession = null;
+        try {
+            //1.获取sqlSession
+            sqlSession = MapperFactory.getSqlSession();
+            //2.获取Dao
+            CompanyDao companyDao = MapperFactory.getMapper(sqlSession, CompanyDao.class);
+            //3.调用Dao层操作
+            PageHelper.startPage(page,size);
+            List<Company> all = companyDao.findAll();
+            PageInfo pageInfo = new PageInfo(all);
+            return pageInfo;
+        }catch (Exception e){
+            //将错误返回最上面
+            throw new RuntimeException(e);
+            //记录日志
+        }finally {
+            try {
+                TransactionUtil.close(sqlSession);
+            }catch (Exception e){
+                //打印异常
+                e.printStackTrace();
+            }
+        }
+    }
+}
